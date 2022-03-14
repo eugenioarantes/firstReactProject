895210c4de06f97c7e16cbcf56f3b7afa5da17a5
Eugenio <eugenilopesarantesjunior@gmail.com>
2022-03-14 16:43:35 -0300

    commit primeiro projeto react

---
+++ first-project-react-eugenio/src/routes/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import {Routes, Route} from 'react-router-dom'
+
+import Dashboard from "../pages/Dashboard";
+
+import Repository from "../pages/Repository";
+
+
+const RoutesApp: React.FC = () => (
+    <Routes>
+        <Route path="/" element={<Dashboard/>}/>
+        <Route path="/repository" element={<Repository/>}/>
+    </Routes>
+); 
+
+export default RoutesApp;
