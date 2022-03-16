e88649d9d470916d52316f80ee31f6a5a93df30b
Eugenio <eugenilopesarantesjunior@gmail.com>
2022-03-16 12:53:47 -0300

    third  day - finishing first project react

--- first-project-react-eugenio/src/pages/Repository/styles.ts
+++ first-project-react-eugenio/src/pages/Repository/styles.ts
@@ -21,3 +21,108 @@ export const Header = styled.header`
     }
     }
 `;
+
+export const RepositoryInfo = styled.section`
+    margin-top: 80px;
+
+    header {
+        display: flex;
+        align-items: center;
+
+        img {
+            width: 120px;
+            height: 120;
+            border-radius: 50%;
+        }
+
+        div {
+            margin-left: 24px;
+
+            strong {
+                font-size: 36px;
+                color: #3d3d4d;
+            }
+
+            p {
+                font-size: 18px;
+                color: #737380;
+                margin-top: 4px;
+
+            }
+
+        }
+
+    }
+
+    ul {
+        display: flex;;
+        list-style: none;
+        margin-top:40px;
+
+        li {
+
+            & + li {
+                margin-left: 80px;
+            }
+
+            strong {
+                display: block;
+                font-size: 36px;
+                color: #3d3d4d;
+            }
+
+            span {
+                display: block;
+                margin-top: 4px;
+                color:#6c6c80;
+            }
+        }
+    }
+`;
+
+export const Issues = styled.div`
+    margin-top: 80px;
+
+    a {
+        background: #FFF;
+        border-radius: 5px;
+        width: 100%;
+        padding: 24px;
+        display: block;
+        text-decoration: none;
+
+        display: flex;
+        align-items: center;
+        transition:  transform 0.2s;
+
+        &:hover {
+            transform:  translate(10px);
+        }
+    
+        & + a {
+            margin-top: 16px;
+        }
+
+        div {
+            margin: 0 16px;
+            flex: 1;
+
+            strong {
+                font-size: 20px;
+                color: #3D3D4D;
+            }
+
+            P {
+                font-size: 18px;
+                color: #a8a8b3;
+                margin-top: 4px;
+            } 
+        }
+
+        svg {
+            margin-left: auto;
+            color: #3D3D4D;
+        }
+
+    }
+`;
